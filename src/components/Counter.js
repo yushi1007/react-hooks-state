3964197f323799628445730af540fe9feed2e9dc
Yu Shi <yushiys95@gmail.com>
2021-04-06 20:43:24 -0400

    commit tree 51942285a4fee5b59ce81472031419aae02fd896

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,11 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <div onClick={increment}>{count}</div>;
 }
 
-export default Counter;
+export default Counter; 
